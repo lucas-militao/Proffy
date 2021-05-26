46cd6566049957293199439268e67698058d5259
lumilitas <lucas23121995>
2021-05-26 13:14:44 -0300

    Implementando rota de criação de classes

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,16 +1,9 @@
 import express from "express";
+import routes from "./routes";
 
 const app = express();
 
 app.use(express.json());
-
-app.post("/users", (request, response) => {
-  const users = [
-    { name: "Lucas Militao", age: 25 },
-    { name: "Lucas Militao", age: 25 },
-  ];
-
-  return response.json(users);
-});
+app.use(routes);
 
 app.listen(3333);
