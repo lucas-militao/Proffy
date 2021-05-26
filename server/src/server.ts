e05b539a0c50f4981e7671f956f8d4d180a1a778
lumilitas <lucas23121995>
2021-05-26 11:12:23 -0300

    Configurado o banco de dados e criada a tabela users

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,1 +1,16 @@
-console.log("Hello world");
+import express from "express";
+
+const app = express();
+
+app.use(express.json());
+
+app.post("/users", (request, response) => {
+  const users = [
+    { name: "Lucas Militao", age: 25 },
+    { name: "Lucas Militao", age: 25 },
+  ];
+
+  return response.json(users);
+});
+
+app.listen(3333);
