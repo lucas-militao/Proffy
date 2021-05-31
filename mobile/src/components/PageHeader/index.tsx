291065d3095af3969f1f50e1b499092493edb7ee
lumilitas <lucas23121995>
2021-05-31 11:48:59 -0300

    Criado e implementado a estilização do componente PageHeader

---
+++ mobile/src/components/PageHeader/index.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { Image, Text, View } from "react-native";
+import { BorderlessButton } from "react-native-gesture-handler";
+
+import backIcon from "../../assets/images/icons/back.png";
+import logoImg from "../../assets/images/logo.png";
+
+import styles from "./styles";
+
+function PageHeader() {
+  function handleGoBack() {}
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.topBar}>
+        <BorderlessButton onPress={handleGoBack}>
+          <Image source={backIcon} resizeMode="contain" />
+        </BorderlessButton>
+
+        <Image source={logoImg} resizeMode="contain" />
+      </View>
+
+      <Text style={styles.title}>Proffys disponíveis</Text>
+    </View>
+  );
+}
+
+export default PageHeader;
